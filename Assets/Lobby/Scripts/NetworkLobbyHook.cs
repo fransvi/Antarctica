17e37a7bb77b059b0c81a78d451adf05b21d117b
Frank <xelimra@gmail.com>
2017-09-06 17:09:31 +0300

    Git fixaus

---
+++ Assets/Lobby/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook : LobbyHook
+{
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponentInChildren<LobbyPlayer>();
+        NetworkPlayerSetup localPlayer = gamePlayer.GetComponent<NetworkPlayerSetup>();
+
+        localPlayer.pName = lobby.playerName;
+        localPlayer.playerColor = lobby.playerColor;
+    }
+}
---
+++ Assets/Scripts/NetworkLobbyHook.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Prototype.NetworkLobby;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkLobbyHook_PlayerSetup : LobbyHook {
+
+    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+    {
+        LobbyPlayer lobby = lobbyPlayer.GetComponentInChildren<LobbyPlayer>();
+        NetworkPlayerSetup localPlayer = gamePlayer.GetComponent<NetworkPlayerSetup>();
+
+        localPlayer.pName = lobby.playerName;
+        localPlayer.playerColor = lobby.playerColor;
+    }
+}
