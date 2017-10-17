252d5c87ebd657cc1c6b246bc0dfeb5ab4bdd90e
Frans <xelimra@gmail.com>
2017-10-17 14:34:30 +0300

    Assetteja projuun + Weather systeemin kehittelyä

---
+++ Assets/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour {
+
+
+    public ParticleSystem _snowParticles;
+    public ParticleSystem _fogParticles;
+    public float _windForce;
+	// Use this for initialization
+	void Start () {
+
+        _snowParticles = transform.Find("SnowParticleSystem").gameObject.GetComponent<ParticleSystem>();
+        _fogParticles = transform.Find("FogParticleSystem").gameObject.GetComponent<ParticleSystem>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+            var rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+            transform.rotation = rotation;
+
+    }
+}
