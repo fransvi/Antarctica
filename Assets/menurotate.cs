1f4ca1aa9aa4876363915ed9243f3ab8601a219b
Frans <xelimra@gmail.com>
2017-12-12 20:20:47 +0200

    Menu ja Lobby hieman graafista päivitystä

---
+++ Assets/menurotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menurotate : MonoBehaviour {
+
+    public float speed = 2f;
+    public float maxRotation = 45f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, maxRotation * Mathf.Sin(Time.time * speed), 0f);
+    }
+}
